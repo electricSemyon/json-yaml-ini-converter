136a3820afb4d493342d1ce67b5246371a580588
electricSemyon <electricSemyon@mail.ru>
2016-08-09 12:08:25 +0300

    Added fixtures, fixed tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,39 +1,34 @@
+'use strict'
 const assert = require('assert');
 const converter = require('../lib/converter');
+const fm = require('../lib/fileManager');
+const expect = require('chai').expect;
+
+let fixtures = [];
+
+fixtures[0] = fm.getFile('./tests/fixtures/1.ini'); //ini fixture
+fixtures[1] = fm.getFile('./tests/fixtures/1.json'); //json fixture
+fixtures[2] = fm.getFile('./tests/fixtures/1.yaml'); //yaml fixture
 
 describe('converter', function () {
   const objectToTest = {
     name: 'Egor',
-    age: 18,
+    age: '18',
     sampleObject: {
       color: 'red',
     },
   };
 
   it('encode testing', function () {
-    assert.equal('{"name":"Egor","age":18,"sampleObject":{"color":"red"}}', converter.encode('json', objectToTest)); // test json
-
-    assert.equal(removeSpaces('name: Egor age: 18 sampleObject: color: red'), // test yaml
-                     removeSpaces(converter.encode('yaml', objectToTest)));
-
-    assert.equal(removeSpaces('name=Egor age=18 [sampleObject] color=red'), // test ini
-                     removeSpaces(converter.encode('ini', objectToTest)));
+    expect(fixtures[2] === converter.encode('yaml', objectToTest)).to.be.true;
+    expect(fixtures[0] === converter.encode('ini', objectToTest)).to.be.true;
+    //expect(fixtures[1] === converter.encode('json', objectToTest)).to.be.true;
   });
 
   it('decode testing', function () {
-    assert.equal(JSON.stringify(objectToTest), // test json. I used JSON.stringify, because of javasrcipt doesn`t allow you to compare two objects :(
-                     JSON.stringify(converter.decode('json', '{"name":"Egor","age":18,"sampleObject":{"color":"red"}}')));
-
-    assert.equal(JSON.stringify(objectToTest),
-                     JSON.stringify(converter.decode('ini', "name='Egor'\r\n" + // ini
-                                                            "age='18'\r\n" +
-                                                            '[sampleObject]\r\n' +
-                                                            'color=red\r\n')));
-    assert.equal(JSON.stringify(objectToTest),
-                     JSON.stringify(converter.decode('yaml', 'name: Egor\r\n' + // yaml
-                                                             'age: 18\r\n' +
-                                                             'sampleObject:\r\n' +
-                                                             '    color: red\r\n')));
+    expect(objectToTest).to.deep.equal(converter.decode('json', fixtures[1]));
+    expect(objectToTest).to.deep.equal(converter.decode('ini',  fixtures[0]));
+    expect(objectToTest).to.deep.equal(converter.decode('yaml', fixtures[2]));
   });
 });
 
